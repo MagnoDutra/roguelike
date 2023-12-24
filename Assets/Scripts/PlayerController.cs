99b3b0d5b296b4d8aaa80dc6346390e63822ae0d
MagnoDutra <jmagno.dutra@hotmail.com>
2023-12-24 12:29:21 -0300

    Arma rotaciona

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    [SerializeField] private Transform gunArm;
+
     [SerializeField] private float moveSpeed;
     private Vector2 moveInput;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,7 +21,15 @@ public class PlayerController : MonoBehaviour
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
+        
+        rb.velocity = moveInput * moveSpeed;
 
-        transform.position += new Vector3(moveInput.x * moveSpeed * Time.deltaTime, moveInput.y * moveSpeed * Time.deltaTime, 0f);
+        Vector3 mousePos = Input.mousePosition; // Posicao do mouse em PIXELS
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition); // Posicao do player transformado em pixels
+
+        Vector2 mouseDistance = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(mouseDistance.y, mouseDistance.x) * Mathf.Rad2Deg; // Atan retorna em radianos, Multiplico pela constante que me devolvera em graus
+
+        gunArm.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
