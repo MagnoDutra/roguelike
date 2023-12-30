1645ea5c7f226cac205f16a494cd014a1ec87bfd
MagnoDutra <jmagno.dutra@hotmail.com>
2023-12-29 21:40:25 -0300

    feito animcao do player e normalizado speed movimento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
 
     [SerializeField] private Transform gunArm;
 
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,12 +23,35 @@ public class PlayerController : MonoBehaviour
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
-        
+
+        moveInput.Normalize();
+
         rb.velocity = moveInput * moveSpeed;
 
+        if (moveInput != Vector2.zero) 
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+        
         Vector3 mousePos = Input.mousePosition; // Posicao do mouse em PIXELS
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition); // Posicao do player transformado em pixels
 
+        if(mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            gunArm.localScale = new Vector3(-1, -1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            gunArm.localScale = new Vector3(1, 1, 1);
+        }
+
+        // Rotate gun
         Vector2 mouseDistance = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
         float angle = Mathf.Atan2(mouseDistance.y, mouseDistance.x) * Mathf.Rad2Deg; // Atan retorna em radianos, Multiplico pela constante que me devolvera em graus
 
