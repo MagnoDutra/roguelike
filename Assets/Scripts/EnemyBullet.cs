d8883e03076d55f7c9dfca151e5f5e3b80f73c35
MagnoDutra <jmagno.dutra@hotmail.com>
2024-01-01 16:26:06 -0300

    Tiro do inimigo implementado

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = PlayerController.instance.transform.position - transform.position;
+        direction.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime); //
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Hurt");
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -18,19 +18,33 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform firePoint;
     [SerializeField] private float fireRate;
+    [SerializeField] private float shootRange;
     private float fireTimer;
+
+    private SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();    
         anim = GetComponent<Animator>();
+        sr = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, PlayerController.instance.transform.position) < rangeToChasePlayer)
+        if (sr.isVisible)
+        {            
+            AttackPlayer();
+        }
+
+        anim.SetBool("isMoving", moveDir != Vector3.zero);
+    }
+
+    private void AttackPlayer()
+    {
+        if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < rangeToChasePlayer)
         {
             moveDir = (PlayerController.instance.transform.position - transform.position).normalized;
         }
@@ -41,9 +55,8 @@ public class EnemyController : MonoBehaviour
 
         rb.velocity = moveDir * moveSpeed;
 
-        anim.SetBool("isMoving", moveDir != Vector3.zero);
 
-        if(shouldShoot && fireTimer <= 0)
+        if (shouldShoot && fireTimer <= 0 && Vector3.Distance(transform.position, PlayerController.instance.transform.position) < shootRange)
         {
             fireTimer = fireRate;
             Instantiate(bullet, firePoint.position, Quaternion.identity);
