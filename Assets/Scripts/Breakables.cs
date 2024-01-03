96d76e32119190bd8b1af798b57b0317ec392a16
MagnoDutra <jmagno.dutra@hotmail.com>
2024-01-03 20:47:16 -0300

    adicionado interacoes de layer entre os objetos

--- Assets/Scripts/Breakables.cs
+++ Assets/Scripts/Breakables.cs
@@ -24,39 +24,49 @@ public class Breakables : MonoBehaviour
         
     }
 
+    public void Smash()
+    {
+        Destroy(gameObject);
+
+        AudioManager.instance.PlaySFX(0);
+
+        // mostra os pedacos da caixa
+        int piecesToDrop = Random.Range(0, maxPieces);
+
+        for (int i = 0; i < piecesToDrop; i++)
+        {
+            int randomPiece = Random.Range(0, brokenPieces.Length);
+
+            Instantiate(brokenPieces[randomPiece], transform.position, transform.rotation);
+        }
+
+        //drop items
+        if (shouldDropItem)
+        {
+            float dropChance = Random.Range(0f, 100f);
+
+            if (dropChance < itemDropPercent)
+            {
+                int randomItem = Random.Range(0, itemsToDrop.Length);
+
+                Instantiate(itemsToDrop[randomItem], transform.position, transform.rotation);
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             if(PlayerController.instance.dashTimer > 0)
             {
-                Destroy(gameObject);
-
-                AudioManager.instance.PlaySFX(0);
-                
-                // mostra os pedacos da caixa
-                int piecesToDrop = Random.Range(0, maxPieces);
-
-                for (int i = 0; i < piecesToDrop; i++)
-                {
-                    int randomPiece = Random.Range(0, brokenPieces.Length);
-
-                    Instantiate(brokenPieces[randomPiece], transform.position, transform.rotation);
-                }
-
-                //drop items
-                if (shouldDropItem)
-                {
-                    float dropChance = Random.Range(0f, 100f);
-
-                    if(dropChance < itemDropPercent)
-                    {
-                        int randomItem = Random.Range(0, itemsToDrop.Length);
-
-                        Instantiate(itemsToDrop[randomItem], transform.position, transform.rotation);
-                    }
-                }
+                Smash();
             }
+        }
+
+        if (other.CompareTag("PlayerBullet"))
+        {
+            Smash();
         }
     }
 }
