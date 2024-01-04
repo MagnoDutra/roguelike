d039cc48aacd4e00bda6c4d38b9ba9299dba7af9
MagnoDutra <jmagno.dutra@hotmail.com>
2024-01-03 23:48:12 -0300

    Porta adicionadä

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    [SerializeField] private bool closeWhenEntered;
+    [SerializeField] private GameObject[] doors;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             CameraController.instance.ChangeTarget(transform);
+
+            if(closeWhenEntered)
+            {
+                foreach(GameObject door in doors)
+                {
+                    door.SetActive(true);
+                }
+            }
         }
     }
 }
