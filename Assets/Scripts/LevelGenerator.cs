ebadc4f4c6039a82d52f1d4db9323ee25283183c
MagnoDutra <jmagno.dutra@hotmail.com>
2024-01-26 10:58:02 -0300

    criado primeira sala do gerador

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject layoutRoom;
+    public Color startColor, endColor;
+    public int distanceToEnd;
+
+    public Transform generatorPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
