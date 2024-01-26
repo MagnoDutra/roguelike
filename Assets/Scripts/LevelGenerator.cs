614e1f1ff961cd5254a2131d372fd57751d01afa
MagnoDutra <jmagno.dutra@hotmail.com>
2024-01-26 11:15:50 -0300

    gerador se move randomicamente

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -10,11 +10,19 @@ public class LevelGenerator : MonoBehaviour
 
     public Transform generatorPoint;
 
+    public enum Direction { up, right, down, left };
+    public Direction selectedDirection;
+
+    public float xOffset = 18;
+    public float yOffset = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         
         Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+        selectedDirection = (Direction)Random.Range(0, 4);
+        MoveGenerationPoint();
         
     }
 
@@ -22,5 +30,24 @@ public class LevelGenerator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void MoveGenerationPoint()
+    {
+        switch (selectedDirection)
+        {
+            case Direction.up:
+                generatorPoint.position += new Vector3(0f, yOffset, 0f);
+                break;
+            case Direction.right:
+                generatorPoint.position += new Vector3(xOffset, 0f, 0f);
+                break;
+            case Direction.down:
+                generatorPoint.position += new Vector3(0f, -yOffset, 0f);
+                break;
+            case Direction.left:
+                generatorPoint.position += new Vector3(-xOffset, 0f, 0f);
+                break;
+        }
     }
 }
